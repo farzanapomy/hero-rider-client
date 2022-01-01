22b0b2e640773e5f8c8c02a05028b7b4fe2b4c73
farzanapomy <farzanapomy56@gmail.com>
2022-01-01 19:27:31 +0600

    added

--- src/pages/Backend/AllLearners/AllLearners.js
+++ src/pages/Backend/AllLearners/AllLearners.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 
 const AllLearners = () => {
     const [learners, setLearners] = useState([])
@@ -6,11 +7,44 @@ const AllLearners = () => {
     useEffect(() => {
         fetch(`https://protected-atoll-65365.herokuapp.com/LearnerDriving`)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setLearners(data))
     }, [])
     return (
-        <div>
-            
+        <div className='container'>
+            <h2>Manage all Orders</h2>
+
+            <Table hover variant="dark" responsive>
+                <thead
+
+                >
+                    <tr>
+                        <th>Index</th>
+                        <th>learner Name</th>
+                        <th>Area</th>
+                        <th>Vehicle Name</th>
+                        <th>Contact</th>
+                    </tr>
+                </thead>
+
+                {learners.map((learner, index) =>
+                    <tbody
+                        key={learner._id}
+                    >
+                        <tr>
+                            <td>{index}</td>
+                            <td>{learner.name}</td>
+                            <td>{learner.area}</td>
+                            <td>{learner.vehicleType}</td>
+                            <td>
+                               {learner.phone}
+                            </td>
+                        </tr>
+
+                    </tbody>
+                )}
+
+            </Table>
+
         </div>
     );
 };
